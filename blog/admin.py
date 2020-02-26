49ac95467b749738131bb3c26f5c6a9120fbb7d4
avengerryan <avengerrmk@gmail.com>
2020-02-26 13:30:33 +0530

    Third push by raw

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,55 @@
 from django.contrib import admin
+from blog.models import (
+            UserType, Company, Category, SubCategory, Blog, BloggerLogin, BlogSeo
+            )
 
 # Register your models here.
+
+
+class UserTypeAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(UserType, UserTypeAdmin)
+
+
+class CompanyAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Company, CompanyAdmin)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Category, CategoryAdmin)
+
+
+class SubCategoryAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(SubCategory, SubCategoryAdmin)
+
+
+class BlogAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Blog, BlogAdmin)
+
+
+class BlogLoginAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(BloggerLogin, BlogLoginAdmin)
+
+
+class BlogSeoAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(BlogSeo, BlogSeoAdmin)
+
+
+
+
+
+
+
