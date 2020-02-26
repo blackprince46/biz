404958e24c6e7984469d59891258e715624d6bbe
avengerryan <avengerrmk@gmail.com>
2020-02-26 15:48:48 +0530

    Fifth push by raw

--- subscription/admin.py
+++ subscription/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from subscription.models import SupportType, SubscriptionPlan, Subscription
 
 # Register your models here.
+
+class SupportTypeAdmin(admin.ModelAdmin):
+    pass
+
+# Instead of this, we can use a decorator.
+admin.site.register(SupportType, SupportTypeAdmin)
+
+
+class SubscriptionAdmin(admin.ModelAdmin):
+    pass
+
+# Instead of this, we can use a decorator.
+admin.site.register(Subscription, SubscriptionAdmin)
+
+
+class SubscriptionPlanAdmin(admin.ModelAdmin):
+    pass
+
+# Instead of this, we can use a decorator.
+admin.site.register(SubscriptionPlan, SubscriptionPlanAdmin)
